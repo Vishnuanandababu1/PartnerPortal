a199e3df650f14c9a4bcba1b8fd969420352aceb
Vishnuanandababu1 <vishnuanandababu@gmail.com>
2024-07-27 13:28:00 +0530

    Initial Commit

---
+++ src/app/library/components/modal/modal.component.ts
@@ -0,0 +1,21 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output  } from '@angular/core';
+
+@Component({
+  selector: 'modal',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './modal.component.html',
+  styleUrl: './modal.component.scss'
+})
+export class ModalComponent {
+  @Input() header!: string;
+  @Input() footer!: string;
+  @Input() customClass!: string;
+  @Input() modalTitle!: string | undefined;
+  @Output() closed = new EventEmitter<void>();
+  
+  closeModal() {
+    this.closed.emit();
+  }
+}
