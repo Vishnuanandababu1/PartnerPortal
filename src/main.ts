b35d520639c4e6a3279d361ba3792cbf656f6ff3
gkv0013 <gkv0013@gmail.com>
2024-07-28 03:16:49 +0530

    translate servises completed, interceptor implemented

--- src/main.ts
+++ src/main.ts
@@ -1,39 +1,28 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { RouterModule } from '@angular/router';
+import { routes } from './app/app.routes';
+import { importProvidersFrom } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
-
-// import { bootstrapApplication } from '@angular/platform-browser';
-// import { provideRouter } from '@angular/router';
-// import { appConfig } from './app/app.config';
-// import { AppComponent } from './app/app.component';
-// import { routes } from './app/app.routes';
-
-// bootstrapApplication(AppComponent, {
-//   providers: [provideRouter(routes)]
-// })
-//   .catch(err => console.error(err));
-
-
-// import { bootstrapApplication } from '@angular/platform-browser';
-// import { provideRouter } from '@angular/router';
-// import { AppComponent } from './app.component';
-// import { routes } from './app.routes';
-
-// bootstrapApplication(AppComponent, {
-//   providers: [provideRouter(routes)]
-// })
-//   .catch(err => console.error(err));
-
-
-// import { bootstrapApplication } from '@angular/platform-browser';
-// import { AppComponent } from './app/app.component';
-// import { provideRouter } from '@angular/router';
-// import { routes } from './app/app.routes';
-
-// bootstrapApplication(AppComponent, {
-//   providers: [provideRouter(routes)]
-// });
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(
+      RouterModule.forRoot(routes),
+      HttpClientModule,
+      TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient]
+        }
+      })
+    )
+  ]
+}).catch(err => console.error(err));
+export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(http, '/i18n/', '.json');
+}
